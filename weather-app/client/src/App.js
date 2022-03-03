07e8ab84fc46d33e8c26989e74be25607b75b030
Lisa Lee <lisawlee04@gmail.com>
2022-03-03 14:43:37 -0800

    Minor updates on App.js.

--- weather-app/client/src/App.js
+++ weather-app/client/src/App.js
@@ -6,25 +6,25 @@ function App() {
   const [day, setDay] = useState({})
     
   {/* useEffect(<function>, <dependency>) */}
+
     useEffect(() => {
       fetch("/api/weather")
       .then((response) => response.json())
       .then((data) => setDay(data.day))
-    }, []) // empty array = this runs on the first render
+    }, [])
 
     return (
     <div className="App">
       <h1>Weather App</h1>
 
-    {/* Q to ask: is this the right check? */}
       {(day === "undefined") ? (
         <p>Loading...</p>
       ) : (
         // otherwise
         <div>
-          {/* <p>{day.main.temp}</p>  */}
           <p>{day.dt_txt}</p>
-          {/* <p>{day.clouds.all}</p> */}
+          {/* <p>{day.main.temp}</p> */}
+          {/* <p>{day.weather[0].description}</p> */}
         </div>
       )
     }
