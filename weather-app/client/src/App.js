2660e0232b7949209de1833ec08633c262cfb816
Lisa Lee <lisawlee04@gmail.com>
2022-03-03 20:17:22 -0800

    Make first day weather info show on the page.

--- weather-app/client/src/App.js
+++ weather-app/client/src/App.js
@@ -6,25 +6,30 @@ function App() {
   const [day, setDay] = useState({})
     
   {/* useEffect(<function>, <dependency>) */}
+// useEffect: pass a function () everything inside the function 
+// executes every time app renders
 
     useEffect(() => {
       fetch("/api/weather")
-      .then((response) => response.json())
+      .then((res) => res.json())
       .then((data) => setDay(data.day))
+      console.log("hello");
     }, [])
+    console.log(day)
 
     return (
     <div className="App">
       <h1>Weather App</h1>
-
-      {(day === "undefined") ? (
+      
+      {(Object.keys(day).length === 0) ? (
         <p>Loading...</p>
       ) : (
-        // otherwise
+        // show: day, date, time, icon, temp, descrip
         <div>
           <p>{day.dt_txt}</p>
-          {/* <p>{day.main.temp}</p> */}
-          {/* <p>{day.weather[0].description}</p> */}
+          <p>Icon Here</p>
+          <p>Temperature: {day.main.temp} &deg;F</p>
+          <p>Description: {day.weather[0].description}</p>
         </div>
       )
     }
