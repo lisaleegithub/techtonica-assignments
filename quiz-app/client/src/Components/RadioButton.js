83a3f8a814406291bb5fce810115880118cf07bf
Lisa Lee <lisawlee04@gmail.com>
2022-03-10 21:24:50 -0800

    Add radio buttons to answer options.

---
+++ quiz-app/client/src/Components/RadioButton.js
@@ -0,0 +1,12 @@
+
+const RadioButton = ({ label, value, onChange }) => {
+  
+    return (
+        <label>
+        <input type="radio" checked={value} onChange={() => onChange(label)} />
+        {label}
+        </label>
+    );
+};
+
+export default RadioButton;
